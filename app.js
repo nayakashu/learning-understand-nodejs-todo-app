8c123b4442dcd23092f158d6825900917fa14550
Ashutosh Nayak <Ashutosh.Nayak@sc.com>
2019-01-04 16:53:36 +0530

    Added CRUD APIs

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var path = require('path');
 var mongoose = require('mongoose');
 var config = require('./config');
 var setupController = require('./controllers/setupController');
+var apiController = require('./controllers/apiController');
 
 var port = process.env.PORT || 3000;
 
@@ -13,6 +14,8 @@ app.use('/assets', express.static(path.join(__dirname, '/public')));
 app.set('view engine', 'ejs');
 
 mongoose.connect(config.getDbConnectionString(), { useNewUrlParser: true });
+
 setupController(app);
+apiController(app);
 
 app.listen(port);
